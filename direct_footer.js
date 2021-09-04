5832cac3b64c88b8e364d46e98a2355ef4c0a09e
popupadsterra <90096023+popupadsterra@users.noreply.github.com>
2021-09-04 20:37:33 +0700

    Update direct_footer.js
    upadate link

--- direct_footer.js
+++ direct_footer.js
@@ -8,5 +8,5 @@ $(document).ready(function()
 	    $(this).removeClass('xepo_ads');
 
 	    //ubah google dengan ads direct link
-	    window.open('https://www.google.com', '_blank');
+	    window.open('https://www.effectivecpmgate.com/rvfgmc9gf0?key=be4775c7458f50033253b8227e1a3d03', '_blank');
 	});
