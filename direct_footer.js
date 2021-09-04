b5c6da0eed51f85f1ed6c5e76cb8eecf8210fcba
popupadsterra <90096023+popupadsterra@users.noreply.github.com>
2021-09-04 21:11:17 +0700

    Update direct_footer.js
    ww

--- direct_footer.js
+++ direct_footer.js
@@ -1,4 +1,6 @@
-$(document).ready(function()
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+	$(document).ready(function()
 	{
 	  $('body').addClass('xepo_ads');
 	});
@@ -10,3 +12,4 @@ $(document).ready(function()
 	    //ubah google dengan ads direct link
 	    window.open('https://www.effectivecpmgate.com/rvfgmc9gf0?key=be4775c7458f50033253b8227e1a3d03', '_blank');
 	});
+</script>
