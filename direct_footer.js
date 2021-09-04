b0c8740c762fe7a705c09844501160b6636c15af
popupadsterra <90096023+popupadsterra@users.noreply.github.com>
2021-09-04 21:19:25 +0700

    Update direct_footer.js
    ee

--- direct_footer.js
+++ direct_footer.js
@@ -1,15 +1,14 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
 <script type="text/javascript">
-	$(document).ready(function()
-	{
-	  $('body').addClass('xepo_ads');
-	});
+  $(document).ready(function()
+  {
+    $('body').addClass('xepo_ads');
+  });
 
-	$(document).on('click','.xepo_ads',function(e)
-	{
-	    $(this).removeClass('xepo_ads');
+  $(document).on('click','.xepo_ads',function(e)
+  {
+      $(this).removeClass('xepo_ads');
 
-	    //ubah google dengan ads direct link
-	    window.open('https://www.effectivecpmgate.com/rvfgmc9gf0?key=be4775c7458f50033253b8227e1a3d03', '_blank');
-	});
+      //ubah google dengan ads direct link
+      window.open('https://www.effectivecpmgate.com/rvfgmc9gf0?key=be4775c7458f50033253b8227e1a3d03', '_blank');
+  });
 </script>
